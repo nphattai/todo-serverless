0f3bc728e3b601629d9902f055a10949577007c3
Tai Nguyen <nphattai98@gmail.com>
2023-05-14 17:33:36 +0700

    feat: add stage to deploy

--- functions/hello.js
+++ functions/hello.js
@@ -1,12 +1,11 @@
 export default function () {
   const appEnv = process.env.APP_ENV;
+
   console.log("Log hello function", appEnv);
 
-  return {
+  return JSON.stringify({
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello world",
-      appEnv,
-    }),
-  };
+    message: "Hello world",
+    appEnv,
+  });
 }
